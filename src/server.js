6785f09c9be0f847a7ab01e58fb5a4e21037d63f
Cinthya Vazquez <cinthyavqza@gmail.comgit config --global user.email cinthyavqza@gmil.comcd>
2024-07-23 21:04:09 -0500

    added .gitignore and Port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
+console.log('Starting server setup...');
 const app = require('./app');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 9090;
 
-app.listen(PORT, () => {
+app.listen(PORT, '127.0.0.1', () => {
   console.log(`Server is running on port ${PORT}`);
 });
+console.log('Server setup complete.');
