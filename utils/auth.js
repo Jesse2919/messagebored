001705c883978e86f6cae18bc569ce6392d692f9
anthonyjj <156860966+anthonyjj@users.noreply.github.com>
2024-07-25 18:05:47 -0700

    added module exports to auth

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,14 @@
 const { GraphQLError } = require('graphql');
 const jwt = require ('jsonwebtoken');
 
+const secret = 'password';
+const expiration = '2h';
+
+module.exports = {
+    AuthenticationErorr: new GraphQLError('Could not authenticate user.', {
+        extensions: {
+            code: 'UNAUTHENTICATED',
+        },
+    }),
+    
+}
