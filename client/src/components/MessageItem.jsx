e770215b11c079d0d4a0b2fe9a19c2aeaaeaaf71
Jesse2919 <jessecorona70@gmail.com>
2024-07-31 19:34:09 -0600

    added save styling

--- client/src/components/MessageItem.jsx
+++ client/src/components/MessageItem.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+//import React from 'react';
 import '../styles/MessageItem.css'; // Import the CSS file
 import '../styles/App.css';
 
 const MessageItem = ({ message, removeMessage }) => (
-    <div>
+    <div className='armadillo'>
         <h4>{message.author}</h4>
         <p>{message.content}</p>
         <button onClick={() => removeMessage(message.id)}>Remove</button>
